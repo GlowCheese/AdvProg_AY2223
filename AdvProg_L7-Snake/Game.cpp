976af40f170fce84ccc6678eb18119d4685925cb
GlowCheese <djnner@proton.me>
2023-03-20 16:27:54 +0700

    Fix bug Lesson 7
    :)

--- AdvProg_L7-Snake/Game.cpp
+++ AdvProg_L7-Snake/Game.cpp
@@ -53,7 +53,7 @@ void Game::processUserInput(Direction direction) {
 bool Game::canChange(Direction current, Direction next) const {
 	if (current == UP || current == DOWN) 
         return next != UP && next != DOWN;
-    return next != LEFT || next != RIGHT;
+    return next != LEFT && next != RIGHT;
 }
 
 
